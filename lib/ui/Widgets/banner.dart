ec9a2ff8263f44422bce355749cb3da76c0367bb
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-06-13 08:27:26 -0500

    add small changes to clean code

--- lib/ui/Widgets/banner.dart
+++ lib/ui/Widgets/banner.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CustomBanner extends StatelessWidget {
-  CustomBanner(this.bannerHeight, {Key? key}) : super(key: key);
-  double bannerHeight;
+  const CustomBanner(this.bannerHeight, {Key? key}) : super(key: key);
+  final double bannerHeight;
 
   @override
   Widget build(BuildContext context) {
